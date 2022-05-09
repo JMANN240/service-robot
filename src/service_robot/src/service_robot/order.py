2f85d8b7d34cf06e7d53b12b4fc04c2f1084ec0d
JT Raber <JMANN240raber@gmail.com>
2022-05-09 11:37:47 -0400

    he works virtually

--- src/service_robot/src/service_robot/order.py
+++ src/service_robot/src/service_robot/order.py
@@ -6,10 +6,11 @@ import math
 
 from std_msgs.msg import Int64
 from geometry_msgs.msg import PoseStamped
+from actionlib_msgs.msg import GoalStatusArray
 
 from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
-from python_qt_binding.QtWidgets import QMainWindow
+from python_qt_binding.QtWidgets import QMainWindow, QListWidgetItem
 
 def makePoseStamped(x, y, z, z_rot, frame_id):
     pos = PoseStamped()
@@ -28,33 +29,92 @@ def makePoseStamped(x, y, z, z_rot, frame_id):
 
     return pos
 
-chef_a_pose = makePoseStamped(-1, 4, 0, 225, 'map')
-chef_b_pose = makePoseStamped(-4, 4, 0, 315, 'map')
-customer_a_pose = makePoseStamped(-1, 1, 0, 135, 'map')
-customer_b_pose = makePoseStamped(-1, 2, 0, 135, 'map')
-customer_c_pose = makePoseStamped(-1, 3, 0, 135, 'map')
-robot_home_pose = makePoseStamped(-4, 1, 0, 45, 'map')
+### CONFIGURATION ###
 
-def tlCallback():
-    goal_publisher.publish(top_left_pose)
+chef_foods = {
+    'chef_a': ['pizza', 'spaghetti'],
+    'chef_b': ['burger']
+}
 
-def blCallback():
-    goal_publisher.publish(bottom_left_pose)
+testing_location = 'MSB_104' # MSB_104 or GAZEBO_HOUSE
 
-def trCallback():
-    goal_publisher.publish(top_right_pose)
-
-def brCallback():
-    goal_publisher.publish(bottom_right_pose)
+if testing_location == 'GAZEBO_HOUSE':
+    poses = {
+        'chef_a': makePoseStamped(-3.2, 4.7, 0, 90, 'map'),
+        'chef_b': makePoseStamped(-1.2, 4.7, 0, 90, 'map'),
+        'customer_a': makePoseStamped(-1, 1, 0, 270, 'map'),
+        'customer_b': makePoseStamped(-2.5, 1, 0, 270, 'map'),
+        'customer_c': makePoseStamped(-4, 1, 0, 270, 'map'),
+        'robot_home': makePoseStamped(-5, 3.5, 0, 0, 'map')
+    }
+elif testing_location == 'MSB_104':
+    poses = {
+        'chef_a': makePoseStamped(-3.2, 4.7, 0, 90, 'map'),
+        'chef_b': makePoseStamped(-1.2, 4.7, 0, 90, 'map'),
+        'customer_a': makePoseStamped(-1, 1, 0, 270, 'map'),
+        'customer_b': makePoseStamped(-2.5, 1, 0, 270, 'map'),
+        'customer_c': makePoseStamped(-4, 1, 0, 270, 'map'),
+        'robot_home': makePoseStamped(-5, 3.5, 0, 0, 'map')
+    }
 
 orders = []
 
-def makeOrderCallback(customer, food):
+location = 'robot_home'
+last_move_status = None
+
+def getChefByFood(food):
+    for chef, foods in chef_foods.items():
+        if food in foods:
+            return chef
+    return None
+
+def updateOrderList(qtlist):
+    global orders
+    for order in orders:
+        qtlist.takeItem(qtlist.row(order['list_item']))
+        print("Removing list item")
+    for order in orders:
+        if order['status'] == 'delivered':
+            continue
+        order['list_item'] = QListWidgetItem(f"{order['customer']}, {order['food']}, {order['chef']}, {order['status']}")
+        qtlist.addItem(order['list_item'])
+        print("Adding list item")
+
+def makeOrderCallback(qtlist, customer, food):
     def cb():
-        global orders
-        orders.append((customer, food))
-        print(orders)
+        global orders, location
+        chef = getChefByFood(food)
+        order = {
+            'customer': customer, 
+            'food': food,
+            'chef': chef,
+            'status': 'ordered',
+            'list_item': QListWidgetItem(f"{customer}, {food}, {chef}, {'ordered'}")
+        }
+        orders.append(order)
+        updateOrderList(qtlist)
+        print(f"{order['customer']} made order of {order['food']} to {order['chef']}")
+        location = chef
+        goal_publisher.publish(poses[location])
     return cb
+
+def pickupOrdersFromChef(chef):
+    global orders
+    for order in orders:
+        if order['chef'] == chef:
+            order['status'] = 'picked up'
+    print(f"Picked up orders from {chef}")
+
+def deliverOrdersToCustomer(customer):
+    global orders
+    for order in orders:
+        if order['customer'] == customer:
+            order['status'] = 'delivered'
+    print(f"Delivered orders to {customer}")
+
+def getOrdersByStatus(status):
+    global orders
+    return [order for order in orders if order['status'] == status]
 
 goal_publisher = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 
@@ -64,18 +124,6 @@ class OrderPlugin(Plugin):
         super(OrderPlugin, self).__init__(context)
         # Give QObjects reasonable names
         self.setObjectName('OrderPlugin')
-
-        # Process standalone plugin command-line arguments
-        from argparse import ArgumentParser
-        parser = ArgumentParser()
-        # Add argument(s) to the parser.
-        parser.add_argument("-q", "--quiet", action="store_true",
-                      dest="quiet",
-                      help="Put plugin in silent mode")
-        args, unknowns = parser.parse_known_args(context.argv())
-        if not args.quiet:
-            print('arguments: ', args)
-            print('unknowns: ', unknowns)
 
         # Create QWidget
         self._widget = QMainWindow()
@@ -91,17 +139,17 @@ class OrderPlugin(Plugin):
         # plugin at once, these lines add number to make it easy to 
         # tell from pane to pane.
 
-        print(dir(self._widget))
+        self._widget.customer_1_pizza_button.clicked.connect(makeOrderCallback(self._widget.orders_list, 'customer_a', "pizza"))
+        self._widget.customer_1_burger_button.clicked.connect(makeOrderCallback(self._widget.orders_list, 'customer_a', "burger"))
+        self._widget.customer_1_spaghetti_button.clicked.connect(makeOrderCallback(self._widget.orders_list, 'customer_a', "spaghetti"))
+        self._widget.customer_2_pizza_button.clicked.connect(makeOrderCallback(self._widget.orders_list, 'customer_b', "pizza"))
+        self._widget.customer_2_burger_button.clicked.connect(makeOrderCallback(self._widget.orders_list, 'customer_b', "burger"))
+        self._widget.customer_2_spaghetti_button.clicked.connect(makeOrderCallback(self._widget.orders_list, 'customer_b', "spaghetti"))
+        self._widget.customer_3_pizza_button.clicked.connect(makeOrderCallback(self._widget.orders_list, 'customer_c', "pizza"))
+        self._widget.customer_3_burger_button.clicked.connect(makeOrderCallback(self._widget.orders_list, 'customer_c', "burger"))
+        self._widget.customer_3_spaghetti_button.clicked.connect(makeOrderCallback(self._widget.orders_list, 'customer_c', "spaghetti"))
 
-        self._widget.customer_1_pizza_button.clicked.connect(makeOrderCallback(1, "pizza"))
-        self._widget.customer_1_burger_button.clicked.connect(makeOrderCallback(1, "burger"))
-        self._widget.customer_1_spaghetti_button.clicked.connect(makeOrderCallback(1, "spaghetti"))
-        self._widget.customer_2_pizza_button.clicked.connect(makeOrderCallback(2, "pizza"))
-        self._widget.customer_2_burger_button.clicked.connect(makeOrderCallback(2, "burger"))
-        self._widget.customer_2_spaghetti_button.clicked.connect(makeOrderCallback(2, "spaghetti"))
-        self._widget.customer_3_pizza_button.clicked.connect(makeOrderCallback(3, "pizza"))
-        self._widget.customer_3_burger_button.clicked.connect(makeOrderCallback(3, "burger"))
-        self._widget.customer_3_spaghetti_button.clicked.connect(makeOrderCallback(3, "spaghetti"))
+        rospy.Subscriber('/move_base/status', GoalStatusArray, self.makeGetStatusCallback())
         
         if context.serial_number() > 1:
             self._widget.setWindowTitle(self._widget.windowTitle() + (' (%d)' % context.serial_number()))
@@ -121,6 +169,50 @@ class OrderPlugin(Plugin):
         # TODO restore intrinsic configuration, usually using:
         # v = instance_settings.value(k)
         pass
+
+    def makeGetStatusCallback(self):
+        def getStatusCallback(status: GoalStatusArray):
+            global orders, last_move_status, location
+
+            # Guard clause: empty status list
+            if len(status.status_list) == 0:
+                return
+
+            # Get the new move status
+            new_move_status = status.status_list[0].status
+            
+            # Guard clause: didn't newly arrive somewhere
+            if new_move_status != 3 or last_move_status == 3:
+                last_move_status = new_move_status
+                return
+
+            print(f"Arrived at {location}!")
+
+            if location == 'chef_a' or location == 'chef_b':
+                pickupOrdersFromChef(location)
+                updateOrderList(self._widget.orders_list)
+            elif location == 'customer_a' or location == 'customer_b' or location == 'customer_c':
+                deliverOrdersToCustomer(location)
+                updateOrderList(self._widget.orders_list)
+
+            print(f"Orders: {orders}")
+
+            ordered_orders = getOrdersByStatus('ordered')
+            picked_up_orders = getOrdersByStatus('picked up')
+            
+            if len(ordered_orders) == 0:
+                if len(picked_up_orders) == 0:
+                    location = 'robot_home'
+                else:
+                    location = picked_up_orders[0]['customer']
+            else:
+                location = ordered_orders[0]['chef']
+            
+            goal_publisher.publish(poses[location])
+            
+            last_move_status = new_move_status
+
+        return getStatusCallback
 
     #def trigger_configuration(self):
         # Comment in to signal that the plugin has a way to configure
